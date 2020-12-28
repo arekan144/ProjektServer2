69cf92263597c45c68b5bfc0dc0484ee84397825
arek <areksa123@o2.pl>
2020-12-28 16:55:00 +0100

    v1

---
+++ server01.js
@@ -0,0 +1,22 @@
+var express = require("express")
+var app = express()
+const PORT = 3000;
+var path = require('path');
+var hbs = require('express-handlebars');
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', hbs({ defaultLayout: 'main.hbs' }));
+app.set('view engine', 'hbs');
+var context = require("./data/data01.json")
+console.log(context)
+
+app.get("/", function (req, res) {
+    res.render('index01.hbs', context);
+})
+
+app.use(express.static('static'))
+
+//nasłuch na określonym porcie
+app.listen(PORT, function () {
+    console.log("start serwera na porcie " + PORT)
+})
